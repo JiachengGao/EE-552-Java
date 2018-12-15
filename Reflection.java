55241ca4fe0b3c5f771ea447930d6a78040024e4
JiachengGao <43893901+JiachengGao@users.noreply.github.com>
2018-12-14 19:13:58 -0500

    Add files via upload
    Java beans reflection API

---
+++ Reflection.java
@@ -0,0 +1,47 @@
+package final_review;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Jiacheng Gao/ Jiawei Wang
+ */
+public class Reflection {
+    public static void getAttributes(Class c) throws NoSuchMethodException {
+        Method[] methods = c.getMethods();
+        ArrayList<String> getters = new ArrayList<>();
+        HashMap<String, Method> setters = new HashMap<>();
+        for (int i = 0; i < methods.length; i++) {
+            String name = methods[i].getName();
+            if (name .startsWith("get"))
+                getters.add(name);
+            else if (name.startsWith("set"))
+                setters.put(name, c.getMethod(name, String.class));
+        }
+        for (int i = 0; i < getters.size(); i++) {
+            String name = getters.get(i).substring(3);
+            Method setter = setters.get(name);
+            if (setter != null) {
+                System.out.println(setter);
+            }
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        String className = "session13.JavaBean1";
+        Class c = Class.forName(className);
+        System.out.println("Your class is named: " + c.getName());
+        Method[] methods = c.getMethods();
+        for (Method m : methods) {
+            System.out.println("method: " + m.getName());
+        }
+        Constructor constructor = c.getConstructor();
+        Object obj = constructor.newInstance();
+
+        Method m = c.getMethod("setName", java.lang.String.class);
+        m.invoke(obj, "abc");
+        getAttributes(c);
+    }
+}
